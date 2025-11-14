4a456b7d39dc473bcda8e221243d4534dbc7bdfc
Elite6802 <kelvimutinda347@gmail.com>
2025-11-14 19:16:30 +0300

    Added IP blocking

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,11 +1,21 @@
 from django.utils.deprecation import MiddlewareMixin
-from .models import RequestLog
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
 from ipware import get_client_ip
 
 class IPLoggingMiddleware(MiddlewareMixin):
+    """
+    Logs incoming requests and blocks IPs in the blacklist.
+    """
+
     def process_request(self, request):
         ip, is_routable = get_client_ip(request)
         if ip:
+            # Check if IP is blocked
+            if BlockedIP.objects.filter(ip_address=ip).exists():
+                return HttpResponseForbidden("Access denied.")
+
+            # Log request
             RequestLog.objects.create(
                 ip_address=ip,
                 path=request.path
